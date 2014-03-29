66c466744ff05e1eef295456a7b586e7a8e35e48
rampmaster <rampmaster@gmail.com>
2014-03-28 23:54:30 -0300

    improve: se agrega soporte faltante para seleccion de usuarios

--- index.php
+++ index.php
@@ -6,22 +6,23 @@
 
 require 'vendor/autoload.php';
 require_once 'q2a/question2answer/qa-include/qa-base.php';
+require_once 'q2a/question2answer/qa-include/qa-db-users.php';
 
 use Symfony\Component\Yaml\Yaml;
 use Symfony\Component\Yaml\Parser;
 use Symfony\Component\Yaml\Exception\ParseException;
 
 try {
-  $config = Yaml::parse(file_get_contents('app/config/config.yml'));
-  $parameters = Yaml::parse(file_get_contents('app/config/parameters.yml'));
+    $config = Yaml::parse(file_get_contents('app/config/config.yml'));
+    $parameters = Yaml::parse(file_get_contents('app/config/parameters.yml'));
 } catch (ParseException $e) {
-  printf("Unable to parse the YAML string: %s", $e->getMessage());
+    printf("Unable to parse the YAML string: %s", $e->getMessage());
 }
 
 if ($config['q2a-loader']) {
-  foreach ($config['q2a-loader'] as $q2aFile) {
-    require_once QA_INCLUDE_DIR . $q2aFile . '.php';
-  }
+    foreach ($config['q2a-loader'] as $q2aFile) {
+        require_once QA_INCLUDE_DIR . $q2aFile . '.php';
+    }
 }
 
 // Display Errors Debug
@@ -48,28 +49,44 @@ $countTwitter = 0;
 $countOpenID = 0;
 
 foreach ($array as $user) {
-  if (!$user['email']) {
-    $i++;
-    if ($user['identity_url']) {
-      $countOpenID++;
-      $usuarios[$user['_id']] = $user['login'].'@openid.migrate';
-    } else if (preg_match('/(_twitter)/i', $user['login'])) {
-      $countTwitter++;
-      $username = str_replace('_twitter', '', $user['login']);
-      $usuarios[$user['_id']] = $username.'@openid.migrate';
+    if (!$user['email']) {
+        $i++;
+        if ($user['identity_url']) {
+            $countOpenID++;
+            $usuarios[$user['_id']] = array(
+                'email' => $user['login'] . '@openid.migrate',
+                'password' => $user['login'] . '@openid.migrate',
+                'login' => $user['login'],
+            );
+        } else if (preg_match('/(_twitter)/i', $user['login'])) {
+            $countTwitter++;
+            $username = str_replace('_twitter', '', $user['login']);
+            $usuarios[$user['_id']] = array(
+                'email' => $username . '@twitter.migrate',
+                'password' => $username . '@twitter.migrate',
+                'login' => $username,
+            );
+        } else {
+            $usuarios[$user['_id']] = array(
+                'email' => $user['login'] . '@shapado.migrate',
+                'password' => $user['login'] . '@shapado.migrate',
+                'login' => $user['login'],
+            );
+        }
     } else {
-      $usuarios[$user['_id']] = $user['login'].'@shapado.migrate';
+        $usuarios[$user['_id']] = array(
+            'email' => $user['email'],
+            'password' => $user['email'],
+            'login' => empty($user['login']) ? $user['email'] : $user['login'],
+        );
     }
-  } else {
-    $usuarios[$user['_id']] = $user['email'];
-  }
+    qa_db_user_create($usuarios[$user['_id']]['email'], $usuarios[$user['_id']]['password'], $usuarios[$user['_id']]['login'], 'QA_USER_LEVEL_BASIC', '127.0.0.1');
 }
 
 echo 'Existen ' . count($usuarios) . ' usuarios con correos' . PHP_EOL
- . $countOpenID . ' usuarios con OpenID' . PHP_EOL
- . $countTwitter . ' usuarios con twitter' . PHP_EOL
- . ($i -$countOpenID - $countTwitter) . ' usuarios sin correo' . PHP_EOL
-;
+    . $countOpenID . ' usuarios con OpenID' . PHP_EOL
+    . $countTwitter . ' usuarios con twitter' . PHP_EOL
+    . ($i - $countOpenID - $countTwitter) . ' usuarios sin correo' . PHP_EOL;
 
 // Get Question Document
 $collection = $db->questions;
@@ -83,43 +100,43 @@ echo "<pre>";
 
 // foreach para cada pregunta
 foreach ($array as $question) {
-  echo $question['title'];
-  echo "<br/>";
-  $type = 'Q'; // question
-  $parentid = null; // does not follow another answer
-  $title = $question['title'];
-  $content = $question['body'];
-  $format = ''; // plain text
-  $categoryid = null; // assume no category
-  $tags = $question['tags'];
-  $userid = qa_get_logged_in_userid();
-
-  // Crea Pregunta
-  $post_id = qa_post_create($type, $parentid, $title, $content, $format, $categoryid, $tags, $userid);
-
-  // Obtiene Document de comentarios (Respuestas que se usaron en Shapado)
-  $collection2 = $db->comments;
-
-  $busqueda2 = array('question_id' => $question['_id']);
-
-  $regs2 = $collection2->find($busqueda2);
-  $array2 = iterator_to_array($regs2);
-
-  foreach ($array2 as $answer) {
-    echo $answer['body'];
+    echo $question['title'];
     echo "<br/>";
-    $type = 'A'; // Awnwer
-    $parentid = $post_id; // Question
-    $title = $answer['title'];
-    $content = $answer['body'];
+    $type = 'Q'; // question
+    $parentid = null; // does not follow another answer
+    $title = $question['title'];
+    $content = $question['body'];
     $format = ''; // plain text
     $categoryid = null; // assume no category
-    $tags = $answer['tags'];
-    $userid = qa_get_logged_in_userid();
+    $tags = $question['tags'];
+    $userid = qa_db_user_find_by_email($usuarios[$question['user_id']]['email'])[0];
 
-    $id_ans = qa_post_create($type, $parentid, $title, $content, $format, $categoryid, $tags, $userid);
-  }
-  echo "<hr/>";
+    // Crea Pregunta
+    $post_id = qa_post_create($type, $parentid, $title, $content, $format, $categoryid, $tags, $userid);
+
+    // Obtiene Document de comentarios (Respuestas que se usaron en Shapado)
+    $collection2 = $db->comments;
+
+    $busqueda2 = array('question_id' => $question['_id']);
+
+    $regs2 = $collection2->find($busqueda2);
+    $array2 = iterator_to_array($regs2);
+
+    foreach ($array2 as $answer) {
+        echo $answer['body'];
+        echo "<br/>";
+        $type = 'A'; // Awnwer
+        $parentid = $post_id; // Question
+        $title = $answer['title'];
+        $content = $answer['body'];
+        $format = ''; // plain text
+        $categoryid = null; // assume no category
+        $tags = $answer['tags'];
+        $userid = qa_db_user_find_by_email($usuarios[$question['user_id']]['email'])[0];
+
+        $id_ans = qa_post_create($type, $parentid, $title, $content, $format, $categoryid, $tags, $userid);
+    }
+    echo "<hr/>";
 }
 
 /*
